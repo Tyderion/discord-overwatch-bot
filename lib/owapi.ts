40dd0df2de11d0e5433a2153576d499de3c2549b
Nadler Gabriel, ENT-DES-BPE-DI-WUI <Gabriel.Nadler@swisscom.com>
2017-03-13 15:18:09 +0100

    Improve types

--- lib/owapi.ts
+++ lib/owapi.ts
@@ -1,6 +1,6 @@
 import { get } from './request';
 
-const getJson = (url: string): Promise<{ data: any }> => {
+const getJson = <T>(url: string): Promise<T> => {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
   return get(url).then(data => {
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "1";
@@ -12,18 +12,18 @@ const getJson = (url: string): Promise<{ data: any }> => {
  * @param  {string} tag
  * @param  {string} region
  */
-export function getCompInfoProfile(tag, region) {
+export function getCompInfoProfile(tag, region): Promise<any> {
   const url = `https://api.lootbox.eu/pc/${region}/${tag.replace('#', '-')}/profile`;
-  return getJson(url).then(r => r.data);
+  return getJson<{ data: any}>(url).then(r => r.data);
 }
 
 /**
  * @param  {string} tag
  * @param  {string} region
  */
-export function getCompInfoAll(tag, region) {
+export function getCompInfoAll(tag, region): Promise<any[]> {
   const url = `https://api.lootbox.eu/pc/${region}/${tag.replace('#', '-')}/competitive/allHeroes/`;
-  return getJson(url);
+  return getJson<any[]>(url);
 }
 
 /**
@@ -32,7 +32,7 @@ export function getCompInfoAll(tag, region) {
  */
 export function getPlayedCompHeroes(tag, region) {
   const url = `https://api.lootbox.eu/pc/${region}/${tag.replace('#', '-')}/competitive/heroes`;
-  return getJson(url);
+  return getJson<{ data: any}>(url);
 }
 const sanitizeNames = name => name.replace('Lúcio', 'Lucio').replace('Torbjörn', 'Torbjoern').replace('Soldier: 76', 'Soldier76');
 
